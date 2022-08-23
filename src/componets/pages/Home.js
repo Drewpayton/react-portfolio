81b3a81a5c570292426b8dd07065656f52fe076e
Drew Payton <drewpayton18@gmail.com>
2022-08-23 13:46:06 -0500

    halfway done with the contact page and I added all my contacts to it. as well as added some minor details to home page

--- src/componets/pages/Home.js
+++ src/componets/pages/Home.js
@@ -11,13 +11,15 @@ export default function Home({ handlePageChange }) {
                 I am a &nbsp; <div class="webdev">Web Developer</div>
             </p>
             <p>
-                <button class="hirebtn">Hire me</button>
-                <a class="projectsbtn"
+                <button class="hirebtn" onClick={() => handlePageChange('Contact')}>Hire me</button>
+                <a class="projectsbtn" rel="noreferrer"
                 onClick={() => handlePageChange('Work')}>Projects</a>
             </p>
         </div>
         <div class="rightimg">
             <img src={portrait} alt="A self portrait of myself"></img>
+            <p class="imgText">Hello, I am currently learning and working as a freelance Web Developer and I am always looking for a challenge. I am 
+            very passionate about the building blocks for the foundation of a website. That drives me to learn more and more everyday.</p>
         </div>
         </>
     )
