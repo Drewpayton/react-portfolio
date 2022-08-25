4fadd8af8913326eaecda4a13635600bf841e6af
Drew Payton <drewpayton18@gmail.com>
2022-08-25 18:09:40 -0500

    finished up the work page for my portfolio and added pictures for the projects

--- src/componets/pages/Work.js
+++ src/componets/pages/Work.js
@@ -1,7 +1,141 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 export default function Work() {
-    return(
-        <div>
-            Welcome to the work page.
-        </div>
-    )
+  return (
+    // <div class="workimg">
+    //   <img src={image1} alt="my project"></img>
+    //   <img src={image1} alt="my project"></img>
+    //   <img src={image1} alt="my project"></img>
+    //   <img src={image1} alt="my project"></img>
+    //   <img src={image1} alt="my project"></img>
+    // </div>
+
+    <div class="workimg">
+      <div class="one">
+        <a
+          href="https://collective-cup.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h4>Collective Cup</h4>
+        </a>
+        <a
+          href="https://github.com/jnmayhook/Project-2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-github"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+        </a>
+      </div>
+      <div class="two">
+        <a
+          href="https://shiver750.github.io/Life-Planner/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h4>Life Planner</h4>
+        </a>
+        <a
+          href="https://github.com/Shiver750/Life-Planner"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-github"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+        </a>
+      </div>
+      <div class="three">
+        <a
+          href="https://drive.google.com/file/d/1F2rlzcWvVivBxs_Iq9P_nkZH_MSSJtKa/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h4>Social Network API</h4>
+        </a>
+        <a
+          href="https://github.com/Drewpayton/Social-Network-API"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-github"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+        </a>
+      </div>
+      <div class="four">
+        <a
+          href="https://desolate-hamlet-44332.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h4>Text Editor</h4>
+        </a>
+        <a
+          href="https://github.com/Drewpayton/Text-Editor"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-github"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+        </a>
+      </div>
+      <div class="five">
+        <a
+          href="https://drive.google.com/file/d/17FPJgLGtGNvR-PJLocCCNUbclHFt5JZf/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h4>e-Commerce Database</h4>
+        </a>
+        <a
+          href="https://github.com/Drewpayton/e-Commerce-Back-end"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            class="bi bi-github"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/componets/portfolioMain.js
+++ src/componets/portfolioMain.js
@@ -1,34 +1,41 @@
-import { useState } from 'react';
-import Home from './pages/Home';
-import Work from './pages/Work';
-import Contact from './pages/Contact';
-import Navbar from './Navbar';
-import Footer from './Footer'
+import { useState } from "react";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import Contact from "./pages/Contact";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function PortfolioMain() {
-const [ currentPage, setCurrentPage ] = useState('Home')
+  const [currentPage, setCurrentPage] = useState("Home");
 
-const handlePageChange = (page) => setCurrentPage(page);
+  const handlePageChange = page => setCurrentPage(page);
 
-const renderPage = () => {
+  const renderPage = () => {
     if (currentPage === "Home") {
-       return <Home handlePageChange={handlePageChange}/>
+      return <Home handlePageChange={handlePageChange} />;
     }
     if (currentPage === "Work") {
-        return <Work />
-     }
-     if (currentPage === "Contact") {
-        return <Contact />
-     }
-}
+      return <Work />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+  };
 
-
-
-return (
-    <>
-    <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-    {renderPage()}
-    < Footer />
-    </>
-)
+  if (currentPage === "Work") {
+    return (
+      <>
+        <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()}
+        <Footer />
+      </>
+    );
+  }
 }
